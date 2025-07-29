8f49aa6ffada9e0d381fa1b3ca90a333e58499a1
ivanMareev <rain12003@yandex.ru>
2025-07-29 19:41:16 +1000

    fix code

--- split_receipt_frontend/vite.config.ts
+++ split_receipt_frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/app/dist', // обязательно путь совпадает с volume
+    outDir: '/app/dist',
     emptyOutDir: true,
   }
 })
